482512b9ee70d8ac61d58e6b82f467e5405c618a
adrian180507 <e-adrdah2@iskule.no>
2024-12-05 13:55:50 +0100

    Lå til tidligere stein saks papir prosjekt

---
+++ prosjekter/steinsakspapir/kode.js
@@ -0,0 +1,41 @@
+
+let playerScore = 0;
+let computerScore = 0;
+let Uavgjortscore = 0;
+
+function playGame(playerChoice) {
+    const choices = ['stein', 'saks', 'papir'];
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
+    let result;
+    let resultElement = document.getElementById("result");
+
+    
+    resultElement.classList.remove("win", "lose", "tie");
+
+    if (playerChoice === computerChoice) {
+        result = "ingenvant!";
+        Uavgjortscore++;
+        resultElement.classList.add("tie");
+        //document.body.style.backgroundColor = white
+        
+    } else if (
+        (playerChoice === 'stein' && computerChoice === 'saks') ||
+        (playerChoice === 'saks' && computerChoice === 'papir') ||
+        (playerChoice === 'papir' && computerChoice === 'stein')
+    ) {
+        result = "Du vinner!";
+        playerScore++;
+        resultElement.classList.add("win");
+    } else {
+        result = "Maskinen vinner!";
+        computerScore++;
+        resultElement.classList.add("lose");
+    }
+    
+
+    resultElement.innerText = `Du valgte ${playerChoice}, maskinen valgte ${computerChoice}. ${result}`;
+    document.getElementById("playerScore").innerText = playerScore;
+    document.getElementById("computerScore").innerText = computerScore;
+    document.getElementById("Uavgjortscore").innerText = Uavgjortscore;
+}
